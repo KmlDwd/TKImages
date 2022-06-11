5990f1cffc8af5949ede750ff8802b4220b38bca
Paradise-blaze <jan4000@o2.pl>
2022-06-11 19:00:11 +0200

    Added Style Format

---
+++ Client/StyleFilterConsumer.py
@@ -0,0 +1,40 @@
+import logging
+import traceback
+
+from Logger.CustomLogFormatter import CustomLogFormatter
+from RabbitMq.Query import ResultResponse
+from RabbitMq.RabbitMQClient import RabbitMQProducer, RabbitMQSyncConsumer, RabbitMQAsyncConsumer
+from StyleFilter.StyleFilter import process_request
+from Utils.Utils import setup_health_consumer
+
+logger = logging.getLogger("StyleFilterConsumer")
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(CustomLogFormatter())
+logger.addHandler(ch)
+
+SERVICE_NAME = "style_service"
+
+if __name__ == '__main__':
+    logger.info("Starting StyleFilterConsumer")
+    consumer = RabbitMQSyncConsumer.from_config('style')
+    producer = RabbitMQProducer.from_config()
+    health_consumer = RabbitMQAsyncConsumer.from_config('health')
+    logger.info("StyleFilterConsumer started successfully")
+
+    logger.info("Starting HealthConsumer")
+    setup_health_consumer(SERVICE_NAME, producer, health_consumer)
+    logger.info("HealthConsumer started successfully")
+
+    def callback(ch, method, properties, body):
+        logger.info(" [x] Received %r" % body)
+        try:
+            result = process_request(body)
+            resp = ResultResponse(200, result, SERVICE_NAME)
+        except Exception as e:
+            logging.error(traceback.format_exc())
+            resp = ResultResponse(500, [], SERVICE_NAME)
+        producer.publish_rmq_message(resp)
+
+    consumer.consume(callback)
--- run_client.bat
+++ run_client.bat
@@ -10,6 +10,7 @@ start cmd /k python SimilarityConsumer.py
 start cmd /k python SizeFilterConsumer.py
 start cmd /k python FacesFilterConsumer.py
 start cmd /k python WeatherFilterConsumer.py
+start cmd /k python StyleFilterConsumer.py
 start cmd /k npm start
 start "server-text" cmd.exe /k "cd ../text_server && mix run ./lib/receive.exs --no-halt"
 start "server-people" cmd.exe /k "cd ../people_server/build/bin && peopleServer.exe"
